9aad9bab69deb0117a345849be461269ccc7c7ce
Akash Mondal <a98mondal@gmail.com>
2025-06-28 13:03:22 +0530

    add umami tracker to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+      <head>
+        {process.env.UMAMI_SCRIPT_URL && <script defer src={process.env.UMAMI_SCRIPT_URL} data-website-id={process.env.UMAMI_DATA_WEBSITE_ID}></script>}
+      </head>
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
